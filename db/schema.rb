7cb498904fb6663e6c67c1a3c6d89c8bd14cff63
mario <mario.rodriguez@softwhisper.es>
2021-10-08 13:22:54 +0200

    migracion de la base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_075633) do
+ActiveRecord::Schema.define(version: 2021_10_08_111356) do
 
   create_table "albums", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
@@ -56,6 +56,18 @@ ActiveRecord::Schema.define(version: 2021_10_01_075633) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["album_id"], name: "index_songs_on_album_id"
+  end
+
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "albums", "groups"
